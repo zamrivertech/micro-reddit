2962f71b7a251ded8fd0cdd892e292cc133eaa53
zamrivertech <mohamadtasul8000@gmail.com>
2026-09-21 08:41:37 +0200

    Create comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_21_063226) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_21_064057) do
+  create_table "comments", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "title"
