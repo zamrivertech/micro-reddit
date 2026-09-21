eeb81e806b255b840cdf15fa5f35e237f6596710
zamrivertech <mohamadtasul8000@gmail.com>
2026-09-21 08:34:57 +0200

    Create user has many posts association and null to true

---
+++ db/migrate/20260921063226_add_user_ref_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUserRefToPosts < ActiveRecord::Migration[8.1]
+  def change
+    add_reference :posts, :user, null: true, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_21_062202) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_21_063226) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "title"
     t.datetime "updated_at", null: false
     t.string "url"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,4 +26,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_21_062202) do
     t.datetime "updated_at", null: false
     t.string "username"
   end
+
+  add_foreign_key "posts", "users"
 end
